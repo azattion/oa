dc471ec7ddf211eccc633148ee77bc3f3afc21f2
razat91 <azat-91knu@mail.ru>
2015-11-03 03:29:47 +0600

    Init project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('header')
+
+@section('content')
+
+    test
+    <ul>
+        <li><a href="#1">Меню</a></li>
+        <li><a href="#2">Меню</a></li>
+        <li><a href="#3">Меню</a></li>
+        <li><a href="#4">Меню</a></li>
+        <li><a href="#5">Меню</a></li>
+    </ul>
+
+@endsection
