ba4a54892123521870f41b19e8ee63283f094a3e
razat91 <azat-91knu@mail.ru>
2015-11-29 04:50:49 +0600

    "Cart "

---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,57 @@
+@extends('partials.content')
+
+@section('content')
+    <div class="container">
+        @if(count($cart))
+            {!! Form::open(['method' => 'PATCH', 'url' => '/cart/store']) !!}
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Название товара</th>
+                    <th>Стоимость</th>
+                    <th>Количество</th>
+                    <th>Сумма</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <? $count = 0; $i = 1; ?>
+                @foreach($cart as $one)
+                    <tr>
+                        <td>{{{$i}}}</td>
+                        <td>{{{$one[0]["name"]}}}</td>
+                        <td>{{{$one[0]["amount"]}}}</td>
+                        <td>
+                            <div class="col-md-6">
+                                {!! Form::hidden('id[]', $one[0]["id"]) !!}
+                                {!! Form::text('count[]', $one[0]["count"], ['placeholder'=>'Количество',
+                                'class'=>'form-control']) !!}
+                            </div>
+                        </td>
+                        <td>
+                            <? $i++; $count += $one[0]["amount"] * $one[0]["count"]; ?>
+                            {{{$one[0]["amount"]*$one[0]["count"]}}} сом.
+                        </td>
+                        <td>
+                            <a href="/cart/destroy/{{{$one[0]["id"]}}}/">Удалить</a>
+                        </td>
+                    </tr>
+                @endforeach
+                <tr>
+                    <td class="text-left" colspan="2"> Количество товаров {{{count($cart)}}}</td>
+                    <td></td>
+                    <td></td>
+                    <td> Итого: {{{$count}}} сом.</td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+            {!! Form::submit('Оформить заказ', ['class'=>'btn primary']) !!}
+            <a href="/" class='btn primary'>Посмотреть еще товары</a>
+            {!! Form::close() !!}
+        @else
+            <div>Корзина пустая</div>
+        @endif
+    </div>
+@endsection
